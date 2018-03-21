4d147597a49e0802d8b05959a987d9d9a24e253e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-21 02:39:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,14 +3,12 @@ class Backer
   attr_accessor :backed_projects, :project
   attr_reader :name
 
-  @backed_projects = [ ]
-
   def initialize(name)
     @name = name
   end
 
   def backed_projects
-    @backed_projects
+    @backed_projects = [ ]
   end
 
   def back_project(project)
