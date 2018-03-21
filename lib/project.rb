fd14b7021ec2abeaf3b0d91d8f82b24cb4c76dc9
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-21 02:42:10 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,14 +2,14 @@ class Project
 
   attr_reader :title
 
-  @backers = []
+  @@backers = []
 
   def initialize(title)
     @title = title
   end
 
   def backers
-    @backers
+    @@backers
   end
 
   def add_backer(backer)
